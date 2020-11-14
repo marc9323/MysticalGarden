624213c3a0fcf052fb946b11f1de03cf0be8b44f
marcholman9323@gmail.com <marcholman9323@gmail.com>
2020-11-14 15:33:50 -0600

    Initial commit

---
+++ android/src/com/youtube/tutorial/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.youtube.tutorial;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.youtube.tutorial.TutorialYouTube;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TutorialYouTube(), config);
+	}
+}
---
+++ desktop/src/com/youtube/tutorial/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.youtube.tutorial.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.youtube.tutorial.TutorialYouTube;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TutorialYouTube(), config);
+	}
+}
